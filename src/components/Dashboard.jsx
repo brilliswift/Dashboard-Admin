14d0f254ec51d0c44f891dbf87013011244b0643
Brilliannissa Zulfa <113734241+brilliswift@users.noreply.github.com>
2025-08-05 20:56:07 +0700

    feat : solve main Dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,25 +1,26 @@
-import React, { useReducer } from 'react';
-import Header from './Header';
-import ResetButton from './ResetButton';
-import StatGrid from './StatGrid';
-import { dashboardReducer, initialState } from '../reducer/dashboardReducer';
-
-
+import React from 'react';
+import { useDashboard } from './hooks/useDashboard';
+import Header from './components/Header';
+import ResetButton from './components/ResetButton';
+import StatGrid from './components/StatGrid';
+import { DASHBOARD_ACTIONS } from './constants';
 
 const Dashboard = () => {
-  const [state, dispatch] = useReducer(dashboardReducer, initialState);
+  const { state, dispatch } = useDashboard();
 
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <div className="max-w-6xl mx-auto">
         {/* Header */}
-        <Header/>
+        <Header />
 
         {/* Tombol Reset */}
-        <ResetButton/>
+        <ResetButton 
+          onReset={() => dispatch({ type: DASHBOARD_ACTIONS.RESET_DATA })} 
+        />
 
         {/* Grid Cards */}
-        <StatGrid/>
+        <StatGrid state={state} dispatch={dispatch} />
 
       </div>
     </div>
