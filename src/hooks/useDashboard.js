5081e511d3254536205caa39320294049046f4f9
Brilliannissa Zulfa <113734241+brilliswift@users.noreply.github.com>
2025-08-05 20:42:12 +0700

    refactor : create useDashboard.js

---
+++ src/hooks/useDashboard.js
@@ -0,0 +1,24 @@
+import { useReducer } from 'react';
+import { dashboardReducer } from '../reducers/dashboardReducer';
+import { INITIAL_DASHBOARD_STATE, DASHBOARD_ACTIONS } from '../constants/dashboardConstants';
+
+export const useDashboard = () => {
+  const [state, dispatch] = useReducer(dashboardReducer, INITIAL_DASHBOARD_STATE);
+
+
+  const actions = {
+    addUser: () => dispatch({ type: DASHBOARD_ACTIONS.ADD_USER }),
+    removeUser: () => dispatch({ type: DASHBOARD_ACTIONS.REMOVE_USER }),
+    addOrder: () => dispatch({ type: DASHBOARD_ACTIONS.ADD_ORDER }),
+    removeOrder: () => dispatch({ type: DASHBOARD_ACTIONS.REMOVE_ORDER }),
+    addProduct: () => dispatch({ type: DASHBOARD_ACTIONS.ADD_PRODUCT }),
+    removeProduct: () => dispatch({ type: DASHBOARD_ACTIONS.REMOVE_PRODUCT }),
+    resetData: () => dispatch({ type: DASHBOARD_ACTIONS.RESET_DATA })
+  };
+
+  return {
+    state,
+    dispatch,
+    actions
+  };
+};
